c792bfc205492afa21aa33e017cb3246d8876213
Jaakko-H <jaheikela@gmail.com>
2020-06-28 12:02:00 +0300

    Extend and tidy api and db layer.

--- db/db.py
+++ db/db.py
@@ -4,20 +4,21 @@ import pymongo
 class MongoDB(object):
     __db = None
     collection_songs = 'songs'
+    collection_song_ratings = 'song_ratings'
 
     def __init__(self, db_config):
         mongo_client = pymongo.MongoClient(db_config['mongodb_url'])
         self.__db = mongo_client[db_config['db_name']]
         self.clear_songs_collection()
-        self.import_data_from_json()
+        self.import_songs_data_from_json()
 
-    def getSongs(self, query, fields_to_return):
+    def get_songs(self, query, fields_to_return):
         return list(self.__db[self.collection_songs].find(query, fields_to_return))
 
     def clear_songs_collection(self):
         self.__db[self.collection_songs].delete_many({})
 
-    def import_data_from_json(self):
+    def import_songs_data_from_json(self):
         with open('songs.json') as songs_file:
             for line in songs_file:
                 self.__db[self.collection_songs].insert_one(json.loads(line))
--- flask_app_song_api.py
+++ flask_app_song_api.py
@@ -1,4 +1,5 @@
 import configparser
+import re
 
 from db import db
 from flask import Flask
@@ -32,6 +33,10 @@ song_fields = {
     'released': fields.String
 }
 
+class SongRating(object):
+    def __init__(self, song_id, rating):
+        pass
+
 class SongList(Resource):
     req_parser = reqparse.RequestParser()
     req_parser.add_argument('page_size')
@@ -39,9 +44,9 @@ class SongList(Resource):
     @marshal_with(song_fields)
     def get(self):
         args = self.req_parser.parse_args()
-        return mongodb.getSongs({}, None)
+        return mongodb.get_songs({}, None)
 
-class SongsByDifficulty(Resource):
+class SongAvgDifficulty(Resource):
     req_parser = reqparse.RequestParser()
     req_parser.add_argument('level')
 
@@ -50,11 +55,34 @@ class SongsByDifficulty(Resource):
         query = {}
         if args['level']:
             query['level'] = int(args['level'])
-        songs = mongodb.getSongs(query, {'difficulty': 1})
-        song_difficulties = [song['difficulty'] for song in songs]
-        if not song_difficulties:
+        songs = mongodb.get_songs(query, {'difficulty': 1})
+        if not songs:
             return 0
+        song_difficulties = [song['difficulty'] for song in songs]
         return mean(song_difficulties)
 
+class SongSearch(Resource):
+    req_parser = reqparse.RequestParser()
+    req_parser.add_argument('message', required=True)
+
+    @marshal_with(song_fields)
+    def get(self):
+        args = self.req_parser.parse_args()
+        searchRegex = re.compile('.*' + args['message'] + '.*', re.IGNORECASE)
+        return mongodb.get_songs(
+                {'$or': [ {'artist': searchRegex}, {'title': searchRegex} ] },
+                None)
+
+class SongRating(Resource):
+    req_parser = reqparse.RequestParser()
+    req_parser.add_argument('song_id', required=True)
+    req_parser.add_argument('rating', required=True)
+
+    def post(self):
+        args = self.req_parser.parse_args()
+
+
+
 api.add_resource(SongList, '/songs')
-api.add_resource(SongsByDifficulty, '/songs/avg/difficulty')
+api.add_resource(SongAvgDifficulty, '/songs/avg/difficulty')
+api.add_resource(SongSearch, '/songs/search')
